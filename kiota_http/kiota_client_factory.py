25d5a845bc07d1bd2674384e4ad5e1e6eff950ea
samwelkanda <samwelkanda@gmail.com>
2022-09-08 02:23:39 +0300

    Update kiota client factory

--- kiota_http/kiota_client_factory.py
+++ kiota_http/kiota_client_factory.py
@@ -3,7 +3,7 @@ from sqlite3 import connect
 
 import httpx
 
-from .middleware import AsyncKiotaTransport #, MiddlewarePipeline, ParametersNameDecodingHandler, RetryHandler
+from .middleware import AsyncKiotaTransport, MiddlewarePipeline, ParametersNameDecodingHandler, RedirectHandler, RetryHandler
 
 
 class KiotaClientFactory:
@@ -20,7 +20,25 @@ class KiotaClientFactory:
         timeout = httpx.Timeout(self.DEFAULT_REQUEST_TIMEOUT, connect=self.DEFAULT_CONNECTION_TIMEOUT)
         kiota_async_client = httpx.AsyncClient(timeout=timeout)
         current_transport = kiota_async_client._transport
+        middleware = self._get_default_middleware(current_transport)
         
-        kiota_async_client._transport = AsyncKiotaTransport(transport = current_transport)
+        kiota_async_client._transport = AsyncKiotaTransport(transport=current_transport, middleware=middleware)
         return kiota_async_client
+
+    def _get_default_middleware(self, transport: httpx.AsyncBaseTransport) -> MiddlewarePipeline:
+        """
+        Helper method that constructs a middleware_pipeline with the specified middleware
+        """
+        middleware_pipeline = MiddlewarePipeline(transport)
+        middlewares = [
+            ParametersNameDecodingHandler(),
+            RetryHandler(),
+            RedirectHandler(),
+        ]
+
+        for middleware in middlewares:
+            middleware_pipeline.add_middleware(middleware)
+            
+        return middleware_pipeline
+    
 

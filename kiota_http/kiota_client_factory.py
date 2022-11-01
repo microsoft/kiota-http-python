a6e72d9309642f5c3eb68bbcccb66c830de758b1
samwelkanda <samwelkanda@gmail.com>
2022-11-01 20:40:49 +0300

    Refactor client factory

--- kiota_http/kiota_client_factory.py
+++ kiota_http/kiota_client_factory.py
@@ -1,52 +1,63 @@
-import functools
-from sqlite3 import connect
+from __future__ import annotations
+
+from typing import List, Optional
 
 import httpx
 
 from .middleware import (
     AsyncKiotaTransport,
+    BaseMiddleware,
     MiddlewarePipeline,
     ParametersNameDecodingHandler,
     RedirectHandler,
     RetryHandler,
 )
 
+DEFAULT_CONNECTION_TIMEOUT: int = 30
+DEFAULT_REQUEST_TIMEOUT: int = 100
+
 
 class KiotaClientFactory:
-    DEFAULT_CONNECTION_TIMEOUT: int = 30
-    DEFAULT_REQUEST_TIMEOUT: int = 100
 
-    def create_with_default_middleware(self) -> httpx.AsyncClient:
+    @staticmethod
+    def create_with_default_middleware() -> httpx.AsyncClient:
         """Constructs native HTTP AsyncClient(httpx.AsyncClient) instances configured with
         a custom transport loaded with a default pipeline of middleware.
 
         Returns:
             httpx.AsycClient: An instance of the AsyncClient object
         """
-        timeout = httpx.Timeout(
-            self.DEFAULT_REQUEST_TIMEOUT, connect=self.DEFAULT_CONNECTION_TIMEOUT
-        )
+        timeout = httpx.Timeout(DEFAULT_REQUEST_TIMEOUT, connect=DEFAULT_CONNECTION_TIMEOUT)
         kiota_async_client = httpx.AsyncClient(timeout=timeout, http2=True)
         current_transport = kiota_async_client._transport
-        middleware = self._get_default_middleware(current_transport)
+        middleware = KiotaClientFactory._get_default_middleware()
+        middleware_pipeline = KiotaClientFactory._create_middleware_pipeline(
+            middleware, current_transport
+        )
 
         kiota_async_client._transport = AsyncKiotaTransport(
-            transport=current_transport, middleware=middleware
+            transport=current_transport, pipeline=middleware_pipeline
         )
         return kiota_async_client
 
-    def _get_default_middleware(self, transport: httpx.AsyncBaseTransport) -> MiddlewarePipeline:
+    @staticmethod
+    def _get_default_middleware() -> List[BaseMiddleware]:
+        """
+        Helper method that returns a list of default middleware
+        """
+        middleware = [RedirectHandler(), RetryHandler(), ParametersNameDecodingHandler()]
+
+        return middleware
+
+    @staticmethod
+    def _create_middleware_pipeline(
+        middleware: Optional[List[BaseMiddleware]], transport: httpx.AsyncBaseTransport
+    ) -> MiddlewarePipeline:
         """
         Helper method that constructs a middleware_pipeline with the specified middleware
         """
         middleware_pipeline = MiddlewarePipeline(transport)
-        middlewares = [
-            ParametersNameDecodingHandler(),
-            RetryHandler(),
-            RedirectHandler(),
-        ]
-
-        for middleware in middlewares:
-            middleware_pipeline.add_middleware(middleware)
-
+        if middleware:
+            for ware in middleware:
+                middleware_pipeline.add_middleware(ware)
         return middleware_pipeline

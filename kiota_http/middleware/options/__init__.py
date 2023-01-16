f4e7f0457449337d8e0b5f9b8dd04524ba8a2247
samwelkanda <samwelkanda@gmail.com>
2023-01-17 01:59:29 +0300

    Update response handler option class

--- kiota_http/middleware/options/__init__.py
+++ kiota_http/middleware/options/__init__.py
@@ -1,4 +1,5 @@
 from .parameters_name_decoding_handler_option import ParametersNameDecodingHandlerOption
 from .redirect_handler_option import RedirectHandlerOption
+from .response_handler_option import ResponseHandlerOption
 from .retry_handler_option import RetryHandlerOption
 from .telemetry_handler_option import TelemetrytHandlerOption
--- kiota_http/middleware/options/response_handler_option.py
+++ kiota_http/middleware/options/response_handler_option.py
@@ -1,13 +1,14 @@
 from typing import Any
+
 from kiota_abstractions.request_option import RequestOption
+
 
 class ResponseHandlerOption(RequestOption):
     """ Adds a ResponseHandler as a RequestOption for the request."""
-    
+
     RESPONSE_HANDLER_OPTION_KEY = "ResponseHandler"
-    
-    def __init__(
-        self, response_handler: Any) -> None:
+
+    def __init__(self, response_handler: Any = None) -> None:
         """To create an instance of ResponseHandlerOption
 
         Args:
@@ -19,6 +20,6 @@ class ResponseHandlerOption(RequestOption):
     def response_handler(self):
         """Property to return the response handler instance"""
         return self._response_handler
-    
+
     def get_key(self):
         return self.RESPONSE_HANDLER_OPTION_KEY

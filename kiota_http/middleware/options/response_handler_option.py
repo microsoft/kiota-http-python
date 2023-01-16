03f093999b926db62008b54aca735ef25f857f82
samwelkanda <samwelkanda@gmail.com>
2023-01-17 01:30:51 +0300

    Add a response handler option class

---
+++ kiota_http/middleware/options/response_handler_option.py
@@ -0,0 +1,24 @@
+from typing import Any
+from kiota_abstractions.request_option import RequestOption
+
+class ResponseHandlerOption(RequestOption):
+    """ Adds a ResponseHandler as a RequestOption for the request."""
+    
+    RESPONSE_HANDLER_OPTION_KEY = "ResponseHandler"
+    
+    def __init__(
+        self, response_handler: Any) -> None:
+        """To create an instance of ResponseHandlerOption
+
+        Args:
+            response_handler (Any): - The response handler instance
+        """
+        self._response_handler = response_handler
+
+    @property
+    def response_handler(self):
+        """Property to return the response handler instance"""
+        return self._response_handler
+    
+    def get_key(self):
+        return self.RESPONSE_HANDLER_OPTION_KEY

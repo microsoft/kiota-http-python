a0db9c4cc00c0632bb1800d4ac48edf9e6293931
Musale Martin <martinmusale@microsoft.com>
2023-07-31 12:30:09 +0300

    Add custom exceptions for kiota http
    Signed-off-by: Musale Martin <martinmusale@microsoft.com>

---
+++ kiota_http/exceptions.py
@@ -0,0 +1,12 @@
+"""Exceptions raised in Kiota HTTP."""
+
+
+class KiotaHTTPException(Exception):
+    """Base class for Kiota HTTP exceptions."""
+
+
+class BackingstoreException(KiotaHTTPException):
+    """Raised for the backing store."""
+
+class DeserializationException(KiotaHTTPException):
+    """Raised for deserialization."""

547993fcd4834d5afc1c0d2f75c5368c67c3b5ab
Martin Musale <martinmusale@microsoft.com>
2023-08-02 13:27:55 +0300

    Add tracing to user_agent_handler
    Signed-off-by: Martin Musale <martinmusale@microsoft.com>

--- kiota_http/middleware/user_agent_handler.py
+++ kiota_http/middleware/user_agent_handler.py
@@ -1,8 +1,14 @@
 from httpx import AsyncBaseTransport, Request, Response
 from kiota_abstractions.request_option import RequestOption
+from opentelemetry import trace
+from opentelemetry.semconv.trace import SpanAttributes
 
+from .._version import VERSION
+from ..observability_options import ObservabilityOptions
 from .middleware import BaseMiddleware
 from .options import UserAgentHandlerOption
+
+tracer = trace.get_tracer(ObservabilityOptions.get_tracer_instrumentation_name(), VERSION)
 
 
 class UserAgentHandler(BaseMiddleware):
@@ -19,14 +25,20 @@ class UserAgentHandler(BaseMiddleware):
         Checks if the request has a User-Agent header and updates it if the
         platform config allows.
         """
-        if self.options and self.options.is_enabled:
-            value = f"{self.options.product_name}/{self.options.product_version}"
+        if options := getattr(request, "options", None):
+            if parent_span := options.get("parent_span", None):
+                _context = trace.set_span_in_context(parent_span)
+                _span = tracer.start_span("redirect_handler_send", _context)
+                if self.options and self.options.is_enabled:
+                    _span.set_attribute("com.microsoft.kiota.handler.useragent.enable", True)
+                    value = f"{self.options.product_name}/{self.options.product_version}"
 
-            user_agent = request.headers.get("User-Agent", "")
-            if not user_agent:
-                request.headers.update({"User-Agent": value})
-            else:
-                if value not in user_agent:
-                    request.headers.update({"User-Agent": f"{user_agent} {value}"})
+                    user_agent = request.headers.get("User-Agent", "")
+                    if not user_agent:
+                        request.headers.update({"User-Agent": value})
+                    else:
+                        if value not in user_agent:
+                            request.headers.update({"User-Agent": f"{user_agent} {value}"})
+                _span.end()
 
         return await super().send(request, transport)

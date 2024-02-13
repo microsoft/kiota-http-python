52dc3e535830a014561cb3a953caf99fe393e64f
samwelkanda <samwelkanda@gmail.com>
2024-02-13 23:34:54 +0300

    Update tests

--- tests/middleware_tests/test_redirect_handler.py
+++ tests/middleware_tests/test_redirect_handler.py
@@ -6,6 +6,13 @@ from kiota_http.middleware.options import RedirectHandlerOption
 
 BASE_URL = 'https://httpbin.org'
 REDIRECT_URL = "https://graph.microsoft.com"
+LOCATION_HEADER: str = "Location"
+AUTHORIZATION_HEADER: str = "Authorization"
+MOVED_PERMANENTLY = 301
+FOUND = 302
+SEE_OTHER = 303
+TEMPORARY_REDIRECT = 307
+PERMANENT_REDIRECT = 308
 
 
 @pytest.fixture
@@ -43,7 +50,7 @@ def test_increment_redirects():
     Tests that redirect are incremented
     """
     request = httpx.Request('GET', BASE_URL)
-    response = httpx.Response(301, request=request)
+    response = httpx.Response(MOVED_PERMANENTLY, request=request)
     history = []
 
     handler = RedirectHandler()
@@ -72,3 +79,196 @@ def test_is_not_https_redirect(mock_redirect_handler):
     url = httpx.URL("http://example.com")
     location = httpx.URL("https://www.example.com")
     assert not mock_redirect_handler.is_https_redirect(url, location)
+    
+@pytest.mark.asyncio
+async def test_ok_response_not_redirected():
+    """Test that a 200 response is not redirected"""
+    def request_handler(request: httpx.Request):
+        return httpx.Response(
+            200,
+        )
+    handler = RedirectHandler()
+    request = httpx.Request(
+        'GET',
+        BASE_URL,
+    )
+    mock_transport = httpx.MockTransport(request_handler)
+    resp = await handler.send(request, mock_transport)
+    assert resp.status_code == 200
+    assert resp.request
+    assert resp.request == request
+    
+@pytest.mark.asyncio
+async def test_redirects_valid():
+    """Test that a valid response is redirected"""
+    def request_handler(request: httpx.Request):
+        if request.url == REDIRECT_URL:
+            return httpx.Response(
+                200,
+            )
+        return httpx.Response(
+            MOVED_PERMANENTLY,
+            headers={LOCATION_HEADER: REDIRECT_URL},
+        )
+    handler = RedirectHandler()
+    request = httpx.Request(
+        'GET',
+        BASE_URL,
+    )
+    mock_transport = httpx.MockTransport(request_handler)
+    resp = await handler.send(request, mock_transport)
+    assert resp.status_code == 200
+    assert resp.request != request
+    assert resp.request.method == request.method
+    assert resp.request.url == REDIRECT_URL
+
+@pytest.mark.asyncio
+async def test_redirect_to_different_host_removes_auth_header():
+    """Test that if a request is redirected to a different host,
+    the Authorization header is removed"""
+    def request_handler(request: httpx.Request):
+        if request.url == REDIRECT_URL:
+            return httpx.Response(
+                200,
+            )
+        return httpx.Response(
+            FOUND,
+            headers={LOCATION_HEADER: REDIRECT_URL},
+        )
+    handler = RedirectHandler()
+    request = httpx.Request(
+        'GET',
+        BASE_URL,
+        headers={AUTHORIZATION_HEADER: "Bearer token"},
+    )
+    mock_transport = httpx.MockTransport(request_handler)
+    resp = await handler.send(request, mock_transport)
+    assert resp.status_code == 200
+    assert resp.request != request
+    assert resp.request.method == request.method
+    assert resp.request.url == REDIRECT_URL
+    assert AUTHORIZATION_HEADER not in resp.request.headers
+
+@pytest.mark.asyncio
+async def test_redirect_on_scheme_change_disabled():
+    """Test that a request is not redirected if the scheme changes and
+    allow_redirect_on_scheme_change is set to False"""
+    def request_handler(request: httpx.Request):
+        if request.url ==  "http://httpbin.org":
+            return httpx.Response(
+                200,
+            )
+        return httpx.Response(
+            TEMPORARY_REDIRECT,
+            headers={LOCATION_HEADER: "http://httpbin.org"},
+        )
+    handler = RedirectHandler()
+    request = httpx.Request(
+        'GET',
+        BASE_URL,
+    )
+    mock_transport = httpx.MockTransport(request_handler)
+    
+    with pytest.raises(Exception):
+        await handler.send(request, mock_transport)
+    
+@pytest.mark.asyncio
+async def test_redirect_on_scheme_change_removes_auth_header():
+    """Test that if a request is redirected to a different scheme,
+    the Authorization header is removed"""
+    def request_handler(request: httpx.Request):
+        if request.url ==  "http://httpbin.org":
+            return httpx.Response(
+                200,
+            )
+        return httpx.Response(
+            TEMPORARY_REDIRECT,
+            headers={LOCATION_HEADER: "http://httpbin.org"},
+        )
+    handler = RedirectHandler(RedirectHandlerOption(allow_redirect_on_scheme_change=True))
+    request = httpx.Request(
+        'GET',
+        BASE_URL,
+        headers={AUTHORIZATION_HEADER: "Bearer token"},
+    )
+    mock_transport = httpx.MockTransport(request_handler)
+    resp = await handler.send(request, mock_transport)
+    assert resp.status_code == 200
+    assert resp.request != request
+    assert AUTHORIZATION_HEADER not in resp.request.headers
+    
+@pytest.mark.asyncio
+async def test_redirect_with_same_host_keeps_auth_header():
+    """Test that if a request is redirected to the same host,
+    the Authorization header is kept"""
+    def request_handler(request: httpx.Request):
+        if request.url ==  "https://httpbin.org/foo":
+            return httpx.Response(
+                200,
+            )
+        return httpx.Response(
+            TEMPORARY_REDIRECT,
+            headers={LOCATION_HEADER: "https://httpbin.org/foo"},
+        )
+    handler = RedirectHandler(RedirectHandlerOption(allow_redirect_on_scheme_change=True))
+    request = httpx.Request(
+        'GET',
+        BASE_URL,
+        headers={AUTHORIZATION_HEADER: "Bearer token"},
+    )
+    mock_transport = httpx.MockTransport(request_handler)
+    resp = await handler.send(request, mock_transport)
+    assert resp.status_code == 200
+    assert resp.request != request
+    assert AUTHORIZATION_HEADER in resp.request.headers
+    
+    
+@pytest.mark.asyncio
+async def test_redirect_with_relative_url_keeps_host():
+    """Test that if a request is redirected to a relative url,
+    the host is kept"""
+    def request_handler(request: httpx.Request):
+        if request.url ==  "https://httpbin.org/foo":
+            return httpx.Response(
+                200,
+            )
+        return httpx.Response(
+            TEMPORARY_REDIRECT,
+            headers={LOCATION_HEADER: "/foo"},
+        )
+    handler = RedirectHandler(RedirectHandlerOption(allow_redirect_on_scheme_change=True))
+    request = httpx.Request(
+        'GET',
+        BASE_URL,
+        headers={AUTHORIZATION_HEADER: "Bearer token"},
+    )
+    mock_transport = httpx.MockTransport(request_handler)
+    resp = await handler.send(request, mock_transport)
+    assert resp.status_code == 200
+    assert resp.request != request
+    assert AUTHORIZATION_HEADER in resp.request.headers
+    assert resp.request.url == "https://httpbin.org/foo"
+    
+@pytest.mark.asyncio
+async def test_max_redirects_exceeded():
+    """Test that if the maximum number of redirects is exceeded, an exception is raised"""
+    def request_handler(request: httpx.Request):
+        if request.url ==  "https://httpbin.org/foo":
+            return httpx.Response(
+                TEMPORARY_REDIRECT,
+                headers={LOCATION_HEADER: "/bar"},
+            )
+        return httpx.Response(
+            TEMPORARY_REDIRECT,
+            headers={LOCATION_HEADER: "/foo"},
+        )
+    handler = RedirectHandler(RedirectHandlerOption(allow_redirect_on_scheme_change=True))
+    request = httpx.Request(
+        'GET',
+        f"{BASE_URL}/foo",
+        headers={AUTHORIZATION_HEADER: "Bearer token"},
+    )
+    mock_transport = httpx.MockTransport(request_handler)
+    with pytest.raises(Exception) as e:
+        await handler.send(request, mock_transport)
+    assert "Too many redirects" in str(e.value)
--- tests/middleware_tests/test_retry_handler.py
+++ tests/middleware_tests/test_retry_handler.py
@@ -8,6 +8,11 @@ from kiota_http.middleware import RetryHandler
 from kiota_http.middleware.options import RetryHandlerOption
 
 BASE_URL = 'https://httpbin.org'
+RETRY_AFTER = "Retry-After"
+RETRY_ATTEMPT = "Retry-Attempt"
+GATEWAY_TIMEOUT = 504
+SERVICE_UNAVAILABLE = 503
+TOO_MANY_REQUESTS = 429
 
 
 def test_no_config():
@@ -28,11 +33,7 @@ def test_custom_options():
     """
     Test that default configuration is overrriden if custom configuration is provided
     """
-    options = RetryHandlerOption()
-    options.max_retry = 1
-    options.max_delay = 100
-    options.should_retry = False
-
+    options = RetryHandlerOption(100, 1, False)
     retry_handler = RetryHandler(options)
 
     assert retry_handler.options.max_retry == 1
@@ -54,7 +55,7 @@ def test_should_retry_valid():
     Test the should_retry method with a valid HTTP method and response code
     """
     request = httpx.Request('GET', BASE_URL)
-    response = httpx.Response(503)
+    response = httpx.Response(SERVICE_UNAVAILABLE)
 
     retry_handler = RetryHandler()
     assert retry_handler.should_retry(
@@ -124,7 +125,7 @@ def test_get_retry_after():
     """
     Test the _get_retry_after method with an integer value for retry header.
     """
-    response = httpx.Response(503, headers={'Retry-After': "120"})
+    response = httpx.Response(SERVICE_UNAVAILABLE, headers={RETRY_AFTER: "120"})
     retry_handler = RetryHandler()
 
     assert retry_handler._get_retry_after(response) == 120
@@ -134,7 +135,7 @@ def test_get_retry_after_no_header():
     """
     Test the _get_retry_after method with no Retry-After header.
     """
-    response = httpx.Response(503)
+    response = httpx.Response(SERVICE_UNAVAILABLE)
 
     retry_handler = RetryHandler()
 
@@ -147,7 +148,128 @@ def test_get_retry_after_http_date():
     """
     timevalue = time() + 120
     http_date = formatdate(timeval=timevalue, localtime=False, usegmt=True)
-    response = httpx.Response(503, headers={'retry-after': f'{http_date}'})
+    response = httpx.Response(SERVICE_UNAVAILABLE, headers={RETRY_AFTER: f'{http_date}'})
 
     retry_handler = RetryHandler()
     assert retry_handler._get_retry_after(response) < 120
+    
+@pytest.mark.asyncio
+async def test_ok_response_not_retried():
+    """Test that a 200 response is not retried"""
+    def request_handler(request: httpx.Request):
+        return httpx.Response(
+            200,
+        )
+    handler = RetryHandler()
+    request = httpx.Request(
+        'GET',
+        'https://localhost',
+    )
+    mock_transport = httpx.MockTransport(request_handler)
+    resp = await handler.send(request, mock_transport)
+    assert resp.status_code == 200
+    assert resp.request
+    assert resp.request == request
+    assert RETRY_ATTEMPT not in resp.request.headers
+    
+@pytest.mark.asyncio
+async def test_retries_valid():
+    """Test that a valid response is retried"""
+    def request_handler(request: httpx.Request):
+        if RETRY_ATTEMPT in request.headers:
+            return httpx.Response(
+                200,
+            )
+        return httpx.Response(
+            SERVICE_UNAVAILABLE,
+        )
+    handler = RetryHandler()
+    request = httpx.Request(
+        'GET',
+        'https://localhost',
+    )
+    mock_transport = httpx.MockTransport(request_handler)
+    resp = await handler.send(request, mock_transport)
+    assert resp.status_code == 200
+    assert RETRY_ATTEMPT in resp.request.headers
+    assert resp.request.headers[RETRY_ATTEMPT] == '1'
+
+@pytest.mark.asyncio
+async def test_should_retry_false():
+    """Test that a request is not retried if should_retry is set to False"""
+    def request_handler(request: httpx.Request):
+        if RETRY_ATTEMPT in request.headers:
+            return httpx.Response(
+                200,
+            )
+        return httpx.Response(
+            TOO_MANY_REQUESTS,
+        )
+    handler = RetryHandler(RetryHandlerOption(10, 1, False))
+    request = httpx.Request(
+        'GET',
+        'https://localhost',
+    )
+    mock_transport = httpx.MockTransport(request_handler)
+    resp = await handler.send(request, mock_transport)
+    assert resp.status_code == TOO_MANY_REQUESTS
+    assert RETRY_ATTEMPT not in resp.request.headers
+
+@pytest.mark.asyncio
+async def test_returns_same_status_code_if_delay_greater_than_max_delay():
+    """Test that a request is delayed based on the Retry-After header"""
+    def request_handler(request: httpx.Request):
+        if RETRY_ATTEMPT in request.headers:
+            return httpx.Response(
+                200,
+            )
+        return httpx.Response(
+            TOO_MANY_REQUESTS,
+            headers={RETRY_AFTER: "20"},
+        )
+    handler = RetryHandler(RetryHandlerOption(10, 1, True))
+    request = httpx.Request(
+        'GET',
+        'https://localhost',
+    )
+    mock_transport = httpx.MockTransport(request_handler)
+    resp = await handler.send(request, mock_transport)
+    assert resp.status_code == 429
+    assert RETRY_ATTEMPT not in resp.request.headers
+    
+@pytest.mark.asyncio
+async def test_retry_options_apply_per_request():
+    """Test that a request options are applied per request"""
+    def request_handler(request: httpx.Request):
+        if "request_1" in request.headers:
+            return httpx.Response(
+                SERVICE_UNAVAILABLE,
+            )
+        return httpx.Response(
+            GATEWAY_TIMEOUT,
+        )
+    handler = RetryHandler(RetryHandlerOption(10, 2, True))
+    
+    # Requet 1
+    request = httpx.Request(
+        'GET',
+        'https://localhost',
+        headers={"request_1": "request_1_header"}
+    )
+    mock_transport = httpx.MockTransport(request_handler)
+    resp = await handler.send(request, mock_transport)
+    assert resp.status_code == SERVICE_UNAVAILABLE
+    assert 'request_1' in resp.request.headers
+    assert resp.request.headers[RETRY_ATTEMPT] == '2'
+    
+    # Request 2
+    request = httpx.Request(
+        'GET',
+        'https://localhost',
+        headers={"request_2": "request_2_header"}
+    )
+    mock_transport = httpx.MockTransport(request_handler)
+    resp = await handler.send(request, mock_transport)
+    assert resp.status_code == GATEWAY_TIMEOUT
+    assert 'request_2' in resp.request.headers
+    assert resp.request.headers[RETRY_ATTEMPT] == '2'
